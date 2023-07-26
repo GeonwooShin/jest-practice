bdb3d1f51b67ebe88ff66cee9d8074315de4a35a
GeonwooShin <rjsdnslawkd@naver.com>
2023-07-26 23:13:14 +0900

    feat: jest 설정

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "jest-environment-jsdom",
+  setupFilesAfterEnv: ["<rootDir>/src/setupTests.ts"],
+  transform: {
+    "^.+\\.ts?$": "ts-jest",
+    "^.+\\.tsx?$": "ts-jest",
+    "^.+\\.js?$": "babel-jest",
+    "^.+\\.jsx?$": "babel-jest",
+  },
+};
