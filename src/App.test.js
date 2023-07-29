1b30642e71dfbef2ea1fd3e258b923ea55d8cf2c
GeonwooShin <rjsdnslawkd@naver.com>
2023-07-29 23:36:26 +0900

    test: 간단 버튼 구현 예제 TDD 적용

--- src/App.test.js
+++ src/App.test.js
@@ -1,12 +1,14 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import App from "./App";
 
-test("버튼이 정확한 초기 색상을 가진다.", () => {
+test("버튼이 적절한 초기색상을 가지고 버튼을 클릭하면 버튼이 파란색으로 변경된다.", () => {
   render(<App />);
-  // button이라는 role을 가지고 텍스트가 Change to Blue인 요소를 가상 DOM에서 찾는다.
   const colorButton = screen.getByRole("button", { name: "Change to Blue" });
-  // button의 배경 색상이 레드임을 단언한다.
   expect(colorButton).toHaveStyle({ backgroundColor: "red" });
+  // button 클릭
+  fireEvent.click(colorButton);
+  // 클릭 후 button의 배경 색상이 블루임을 단언한다.
+  expect(colorButton).toHaveStyle({ backgroundColor: "blue" });
+  // button의 텍스트 또한 Change to Red임을 단언한다.
+  expect(colorButton).toHaveTextContent("Change to Red");
 });
-
-test("클릭하면 버튼이 파란색으로 변경된다.", () => {});
