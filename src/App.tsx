be306b777aa1f619393658eae1211f3f101a8fbe
GeonwooShin <rjsdnslawkd@naver.com>
2023-07-29 23:37:05 +0900

    feat: TDD 적용하여 테스트 통과되도록 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
-import React from "react";
+import { useState } from "react";
 
 const App = () => {
+  const [isClicked, setIsCliked] = useState<boolean>(false);
+  const btnText = isClicked ? "Red" : "Blue";
+  const handleButton = () => {
+    setIsCliked(!isClicked);
+  };
   return (
     <div>
-      <button style={{ backgroundColor: "red" }}>Change to Blue</button>
+      <button
+        style={
+          isClicked ? { backgroundColor: "blue" } : { backgroundColor: "red" }
+        }
+        onClick={handleButton}
+      >
+        Change to {btnText}
+      </button>
     </div>
   );
 };
