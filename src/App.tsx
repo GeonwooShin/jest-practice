b35c3c275208202356e4b8582483ad0e9a4644b2
GeonwooShin <rjsdnslawkd@naver.com>
2023-07-30 18:18:15 +0900

    feat: 체크박스 체크 유무에 따른 버튼 비활성화 구현

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 
 const App = () => {
   const [isClicked, setIsCliked] = useState<boolean>(false);
+  const [isDisabled, setIsDisabled] = useState<boolean>(false);
   const btnText = isClicked ? "Red" : "Blue";
   const handleButton = () => {
     setIsCliked(!isClicked);
@@ -13,9 +14,14 @@ const App = () => {
           isClicked ? { backgroundColor: "blue" } : { backgroundColor: "red" }
         }
         onClick={handleButton}
+        disabled={isDisabled}
       >
         Change to {btnText}
       </button>
+      <input
+        type="checkbox"
+        onChange={(e) => setIsDisabled(e.target.checked)}
+      />
     </div>
   );
 };
