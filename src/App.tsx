c40260c86615e1ae9c2f58bacc8180f64cc1fb2b
GeonwooShin <rjsdnslawkd@naver.com>
2023-08-01 23:12:29 +0900

    refactor: App 페이지 초기화

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,4 @@
-import { useState } from "react";
-
-export function replaceCamelWithSpaces(colorName: string) {
-  return colorName.replace(/\B([A-Z])\B/g, " $1");
-}
-
 const App = () => {
-  const [isClicked, setIsCliked] = useState<boolean>(false);
-  const [isDisabled, setIsDisabled] = useState<boolean>(false);
-  const btnText = isClicked ? "Red" : "Blue";
-  const handleButton = () => {
-    setIsCliked(!isClicked);
-  };
-  return (
-    <div>
-      <button
-        style={{
-          backgroundColor: isDisabled ? "gray" : isClicked ? "blue" : "red",
-        }}
-        onClick={handleButton}
-        disabled={isDisabled}
-      >
-        Change to {btnText}
-      </button>
-      <label>
-        <input
-          type="checkbox"
-          onChange={(e) => setIsDisabled(e.target.checked)}
-        />
-        버튼 비활성화
-      </label>
-    </div>
-  );
+  return <div></div>;
 };
 export default App;
