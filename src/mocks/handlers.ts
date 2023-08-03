dec7d8146101354b51013a5d467f808cd00f323a
GeonwooShin <rjsdnslawkd@naver.com>
2023-08-03 22:28:14 +0900

    feat: toppings 관련 msw 설정

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -4,10 +4,21 @@ export const handlers = [
   rest.get("http://localhost:3030/scoops", (req, res, ctx) => {
     return res(
       ctx.json([
-        { name: "Chocolate", imagePath: "/images/chocolate.png" },
-        { name: "Vanilla", imagePath: "/images/vanilla.png" },
-        { name: "Mint Chocolate", imagePath: "/images/mintChocolate.png" },
-        { name: "Strawberry", imagePath: "/images/strawberry.png" },
+        { name: "Chocolate", imagePath: "images/chocolate.png" },
+        { name: "Vanilla", imagePath: "images/vanilla.png" },
+        { name: "Mint Chocolate", imagePath: "images/mintChocolate.png" },
+        { name: "Strawberry", imagePath: "images/strawberry.png" },
+      ])
+    );
+  }),
+  rest.get("http://localhost:3030/toppings", (req, res, ctx) => {
+    return res(
+      ctx.json([
+        { name: "Cherries", imagePath: "images/cherries.png" },
+        { name: "M&Ms", imagePath: "images/m-and-ms.png" },
+        { name: "Cookies", imagePath: "images/cookies.png" },
+        { name: "Jellies", imagePath: "images/jellies.png" },
+        { name: "Splinkles", imagePath: "images/splinkles.png" },
       ])
     );
   }),
