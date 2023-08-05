017876ed780e885b0cb228fb5364b8a98ca9a1f7
GeonwooShin <rjsdnslawkd@naver.com>
2023-08-05 23:44:48 +0900

    feat: 토핑 & 스쿱 추가 로직 작성

---
+++ src/pages/entry/ToppingOption.jsx
@@ -0,0 +1,23 @@
+import { Col, FormGroup, Form } from "react-bootstrap";
+import { useOrderDetails } from "../../contexts/OrderDetails";
+
+const ToppingOption = ({ name, imagePath }) => {
+  const { updateItemCount } = useOrderDetails();
+  const handleChange = (e) => {
+    updateItemCount(name, e.target.checked ? 1 : 0, "toppings");
+  };
+  return (
+    <Col xs={12} sm={6} md={4} lg={3} style={{ textAlign: "center" }}>
+      <img
+        style={{ width: "75%" }}
+        src={`http://localhost:3030/${imagePath}`}
+        alt={`${name} topping`}
+      />
+      <FormGroup controlId={`${name}-topping-checkbox`}>
+        <Form.Check type="checkbox" onChange={handleChange} label={name} />
+      </FormGroup>
+    </Col>
+  );
+};
+
+export default ToppingOption;
