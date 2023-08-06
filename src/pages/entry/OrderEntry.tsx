1ea2bd15c1860a10ac8987e781e72b68a4c4011c
GeonwooShin <rjsdnslawkd@naver.com>
2023-08-06 17:57:29 +0900

    feat: 총 합계 작성

--- src/pages/entry/OrderEntry.tsx
+++ src/pages/entry/OrderEntry.tsx
@@ -1,10 +1,13 @@
+import { useOrderDetails } from "../../contexts/OrderDetails";
 import Options from "./Options";
 
 const OrderEntry = () => {
+  const { totals } = useOrderDetails();
   return (
     <div>
       <Options optionType="scoops" />
       <Options optionType="toppings" />
+      <h2>총 합계: {totals.scoops + totals.toppings}</h2>
     </div>
   );
 };
