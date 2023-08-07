bae1a309719f73a69cdc85977592595f7be959d1
GeonwooShin <rjsdnslawkd@naver.com>
2023-08-07 23:30:10 +0900

    feat: post mock api 추가

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,5 +1,13 @@
 import { rest } from "msw";
 
+function sleep() {
+  return new Promise((resolve) =>
+    setTimeout(() => {
+      resolve(1);
+    }, 100)
+  );
+}
+
 export const handlers = [
   rest.get("http://localhost:3030/scoops", (req, res, ctx) => {
     return res(
@@ -20,6 +28,14 @@ export const handlers = [
         { name: "Jellies", imagePath: "images/jellies.png" },
         { name: "Splinkles", imagePath: "images/splinkles.png" },
       ])
+    );
+  }),
+  rest.post("http://localhost:3030/order", async (req, res, ctx) => {
+    await sleep();
+    return res(
+      ctx.json({
+        orderNumber: 123455661,
+      })
     );
   }),
 ];
