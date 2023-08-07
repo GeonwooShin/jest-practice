f49aea4ea65f5b62d0aee28bfa12660a5321f31f
GeonwooShin <rjsdnslawkd@naver.com>
2023-08-07 23:33:20 +0900

    test: jest mock 함수 추가

--- src/pages/summary/test/SummaryForm.test.tsx
+++ src/pages/summary/test/SummaryForm.test.tsx
@@ -3,7 +3,7 @@ import userEvent from "@testing-library/user-event";
 import SummaryForm from "../SummaryForm";
 
 test("체크박스 디폴트 값은 체크되지 않은 체크박스이다.", () => {
-  render(<SummaryForm />);
+  render(<SummaryForm setOrderPhase={jest.fn()} />);
   const checkbox = screen.getByRole("checkbox", {
     name: "약관 확인에 동의합니다.",
   });
@@ -14,7 +14,7 @@ test("체크박스 디폴트 값은 체크되지 않은 체크박스이다.", ()
 
 test("체크박스가 체크되면 버튼이 활성화되고 체크되지 않으면 비활성화 된다.", async () => {
   const user = userEvent.setup();
-  render(<SummaryForm />);
+  render(<SummaryForm setOrderPhase={jest.fn()} />);
   const button = screen.getByRole("button", { name: "주문 확인" });
   const checkbox = screen.getByRole("checkbox", {
     name: "약관 확인에 동의합니다.",
@@ -27,7 +27,7 @@ test("체크박스가 체크되면 버튼이 활성화되고 체크되지 않으
 
 test("마우스 커서를 올리면 팝오버 된다.", async () => {
   const user = userEvent.setup();
-  render(<SummaryForm />);
+  render(<SummaryForm setOrderPhase={jest.fn()} />);
   // 팝오버가 처음에는 숨겨져있다.
   const nullPopover = screen.queryByText(
     "아이스크림이 실제로 배달되지는 않습니다."
