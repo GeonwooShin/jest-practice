9187f77af41667a7b8c38cbddcfb00ad0777b9ec
GeonwooShin <rjsdnslawkd@naver.com>
2023-08-07 23:29:12 +0900

    feat: orderPhase에 따른 컴포넌트 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,31 @@
+import { useState } from "react";
 import { Container } from "react-bootstrap";
+import OrderConfirmation from "./pages/confirmation/OrderConfirmation";
 import OrderEntry from "./pages/entry/OrderEntry";
+import OrderSummary from "./pages/summary/OrderSummary";
 import { OrderDetailsProvider } from "./contexts/OrderDetails";
 
 const App = () => {
+  const [orderPhase, setOrderPhase] = useState<string>("inProgress");
+  let Component: any = OrderEntry;
+  switch (orderPhase) {
+    case "inProgress":
+      Component = OrderEntry;
+      break;
+    case "review":
+      Component = OrderSummary;
+      break;
+    case "completed":
+      Component = OrderConfirmation;
+      break;
+    default:
+  }
   return (
-    <Container>
-      <OrderDetailsProvider>
-        <OrderEntry />
-      </OrderDetailsProvider>
-    </Container>
+    <OrderDetailsProvider>
+      <Container>
+        <Component setOrderPhase={setOrderPhase} />
+      </Container>
+    </OrderDetailsProvider>
   );
 };
 export default App;
