38123b6de4961f38a2795f488a1b4e999aa120d2
GeonwooShin <rjsdnslawkd@naver.com>
2023-08-07 23:30:58 +0900

    feat: 주문 초기 페이지 작성

--- src/pages/entry/OrderEntry.tsx
+++ src/pages/entry/OrderEntry.tsx
@@ -1,13 +1,23 @@
+import { SetStateAction, Dispatch } from "react";
 import { useOrderDetails } from "../../contexts/OrderDetails";
 import Options from "./Options";
+import { Button } from "react-bootstrap";
 
-const OrderEntry = () => {
+const OrderEntry = ({
+  setOrderPhase,
+}: {
+  setOrderPhase: Dispatch<SetStateAction<string>>;
+}) => {
   const { totals } = useOrderDetails();
+  const handleClick = () => {
+    setOrderPhase("review");
+  };
   return (
     <div>
       <Options optionType="scoops" />
       <Options optionType="toppings" />
       <h2>총 합계: {totals.scoops + totals.toppings}</h2>
+      <Button onClick={handleClick}>주문</Button>
     </div>
   );
 };
