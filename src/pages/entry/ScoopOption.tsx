ff0be39afa55f5f266a735076f18a411bdf6c442
GeonwooShin <rjsdnslawkd@naver.com>
2023-08-08 22:16:07 +0900

    refactor: 유효한 값 설정

--- src/pages/entry/ScoopOption.tsx
+++ src/pages/entry/ScoopOption.tsx
@@ -1,5 +1,6 @@
 import { Col, Form, Row } from "react-bootstrap";
 import { useOrderDetails } from "../../contexts/OrderDetails";
+import { useState } from "react";
 
 const ScoopOption = ({
   name,
@@ -8,9 +9,19 @@ const ScoopOption = ({
   name: string;
   imagePath: string;
 }) => {
+  const [isValid, setIsValid] = useState<boolean>(true);
   const { updateItemCount } = useOrderDetails();
-  const handleChange = (e: any) =>
-    updateItemCount(name, parseInt(e.target.value), "scoops");
+  const handleChange = (e: any) => {
+    const currentValue = e.target.value;
+    const currentValueFloat = parseFloat(currentValue);
+    const valueIsValid =
+      0 <= currentValueFloat &&
+      currentValueFloat <= 10 &&
+      Math.floor(currentValueFloat) === currentValueFloat;
+    setIsValid(valueIsValid);
+    const newValue = valueIsValid ? parseInt(currentValue) : 0;
+    updateItemCount(name, newValue, "scoops");
+  };
   return (
     <Col xs={12} sm={6} md={4} lg={3} style={{ textAlign: "center" }}>
       <img
@@ -31,6 +42,7 @@ const ScoopOption = ({
             type="number"
             defaultValue={0}
             onChange={handleChange}
+            isInvalid={!isValid}
           ></Form.Control>
         </Col>
       </Form.Group>
